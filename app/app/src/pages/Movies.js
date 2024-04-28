41cb85fdb41ca1c216df7c9d59e324493e89a30a
Paige Knowles <paknowles@student.fullsail.edu>
2024-04-27 22:28:10 -0400

    update package.json

--- app/app/src/pages/Movies.js
+++ app/app/src/pages/Movies.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
+import axios from '../axios.config'; 
 import { useParams } from 'react-router-dom';
 import MovieHeader from '../components/MovieHeader';
 
