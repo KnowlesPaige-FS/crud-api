e050c648dc5666b2566905effcf19070840a57f8
Paige Knowles <paknowles@student.fullsail.edu>
2024-04-27 21:45:52 -0400

    updat app.js

--- server/app/app.js
+++ server/app/app.js
@@ -65,8 +65,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
 })
     .then(() => {
         console.log("MongoDB connected...");
