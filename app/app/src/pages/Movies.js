c3400f21241bfda67a9753e003d87d8d6400d8ec
KnowlesPaige-FS <129763432+KnowlesPaige-FS@users.noreply.github.com>
2024-05-01 22:29:07 -0400

    Update Movies.js
    Functionality for delete and update to movies.js

--- app/app/src/pages/Movies.js
+++ app/app/src/pages/Movies.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios.config'; 
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import MovieHeader from '../components/MovieHeader';
+import MovieModal from '../components/MovieModal';
 
 const Movies = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -21,39 +23,71 @@ const Movies = () => {
     fetchMovieDetails();
   }, [id]);
 
+  const navigate = useNavigate(); 
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/movies/${id}`);
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.error('Failed to delete movie', error);
+    }
+  };
+
+  const handleUpdate = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleUpdateMovie = async (updatedMovie) => {
+    try {
+      await axios.put(`/movies/${id}`, updatedMovie);
+      setShowModal(false);
+      setMovie(updatedMovie);
+    } catch (error) {
+      console.error('Failed to update movie', error);
+    }
+  };
+
   if (!movie) {
     return <div>Loading...</div>;
   }
 
   return (
     <section style={styles.container}>
-      <MovieHeader
-        backgroundImage={movie.backdrop_path}
-      />
+      <MovieHeader backgroundImage={movie.backdrop_path} />
       <section style={styles.movieContent} className="d-flex flex-row justify-content-around align-items-center">
         <div style={styles.posterImg}>
           <div style={styles.overlay}></div>
           <img style={styles.img} src={movie.poster_path} alt='Movie Poster' />
         </div>
         <div style={styles.desc}>
-            <div style={styles.headings}>
-              <h2>{movie.title}</h2>
-              <h5>Release Date: {movie.release_date}</h5>
-            </div>
-            
-            <div style={styles.overview}>
-                <h3>Overview:</h3>
-                <p style={styles.p}>{movie.overview}</p>  
-                <h5>{movie.genre_id}</h5>
-            </div>
-            
+          <div style={styles.headings}>
+            <h2>{movie.title}</h2> 
+            <h5>Release Date: {movie.release_date}</h5>  
+            <h5>Genre: {movie.genre[0]}</h5>
+          </div>
+          <div style={styles.overview}>
+            <h3>Overview:</h3>
+            <p style={styles.p}>{movie.overview}</p>  
+          </div>
+          <div style={styles.btns}>
+            <button onClick={handleUpdate} style={styles.updateBtn}>Update Movie</button>
+            <button onClick={handleDelete} style={styles.deleteBtn}>Delete Movie</button>
+          </div>
         </div>
       </section>
+      <MovieModal movie={movie} onUpdate={handleUpdateMovie} onClose={handleCloseModal} showModal={showModal} />
     </section>
   );
 }
 
 export default Movies;
+
+
 const styles = {
         movieContent: {
             padding: '2% 0',
@@ -61,7 +95,6 @@ const styles = {
             textAlign: 'center'
         },
         posterImg: {
-            // width: '350px',
             width: '25%',
             height: '550px',
             position: 'relative',
@@ -91,6 +124,23 @@ const styles = {
             margin: '0 0 4%'
         },
         overview: {
-            marign: '2% 0'
+            margin: '2% 0'
+        },
+        btns: { 
+          margin: '8% 0'
+        },
+        updateBtn: {
+          border: 'none',
+          background: 'transparent',
+          fontVariant: 'small-caps',
+          fontWeight: 'bold',
+          color: '#9F622D'       
+        },
+        deleteBtn: {
+          border: 'none',
+          background: 'transparent',
+          fontVariant: 'small-caps',
+          fontWeight: 'bold',
+          color: '#611E17'
         }
     };
